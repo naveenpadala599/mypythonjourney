b0d9a4cf4fee84a6b3c0973bb9cb8fcd725911f4
naveenpadala599 <58857810+naveenpadala599@users.noreply.github.com>
2025-07-21 10:13:13 +0530

    Create another_trying_different_formating_styles.py

---
+++ another_trying_different_formating_styles.py
@@ -0,0 +1,5 @@
+x = 5.56
+
+print("The square root of %.1f is %.3f" % (x, x**0.5))
+print("The square root of {0:.1f} is {1:.3f}".format(x, x**0.5))
+print(f"The square root of {x:.1f} is {x**0.5:.3f}")
